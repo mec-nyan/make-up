d1c6695e25427167ad0010e0e52b30a7f42f6f03
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-11 21:05:18 -0300

    Polish some stuff.

--- go/cmd/simple_explorer/main.go
+++ go/cmd/simple_explorer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/charmbracelet/bubbles/list"
@@ -11,16 +12,16 @@ import (
 
 var (
 	leftStyle = lipgloss.NewStyle().
-		Width(30).
-		Height(40).
-		Padding(1, 2, 1, 2)
+			Width(30).
+			Height(40).
+			Padding(1, 2, 1, 2)
 
 	rightStyle = lipgloss.NewStyle().
-		Border(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("#89dceb")).
-		Padding(1, 2).
-		Height(40).
-		Width(50)
+			Border(lipgloss.NormalBorder()).
+			BorderForeground(lipgloss.Color("#89dceb")).
+			Padding(1, 2).
+			Height(40).
+			Width(50)
 )
 
 type (
@@ -32,7 +33,7 @@ type (
 	}
 )
 
-func (i Item) Title() string       { return i.Name }
+func (i Item) Title() string { return i.Name }
 
 func (i Item) Description() string { return i.Desc }
 
@@ -78,6 +79,30 @@ func getSampleItems() []Item {
 	}
 }
 
+type simpleDelegate struct{}
+
+func (d simpleDelegate) Height() int { return 1 }
+
+func (d simpleDelegate) Spacing() int { return 1 }
+
+func (d simpleDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+
+func (d simpleDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
+	it, ok := item.(Item)
+	if !ok {
+		return
+	}
+
+	str := it.Title()
+	if index == m.Index() {
+		str = lipgloss.NewStyle().Foreground(lipgloss.Color("#fab387")).Render("> " + str)
+	} else {
+		str = "  " + str
+	}
+
+	fmt.Fprintf(w, "%s", str)
+}
+
 func main() {
 	var items []list.Item
 	for _, i := range getSampleItems() {
@@ -85,12 +110,16 @@ func main() {
 	}
 
 	const defaultWidth = 30
-	l := list.New(items, list.NewDefaultDelegate(), defaultWidth, 40)
+	l := list.New(items, simpleDelegate{}, defaultWidth, 40)
+	l.SetShowHelp(false)
+	l.SetShowTitle(false)
+	l.SetShowPagination(false)
+	l.SetShowStatusBar(false)
 	l.Title = "Stuff"
 
 	m := model{list: l}
 
-	p := tea.NewProgram(m)
+	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Oops! We fucked up:", err)
 		os.Exit(1)
